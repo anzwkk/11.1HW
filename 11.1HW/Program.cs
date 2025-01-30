72915f2366d5346ea6273e6f4c3ff304d1a94465
anzwkk <anam89737@gmail.com>
2025-01-30 23:00:51 +0300

    Added initial logic for the game

--- 11.1HW/Program.cs
+++ 11.1HW/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string secretWord = "Хілель";
+        char[] guessedWord = new char[secretWord.Length];
+
+        for (int i = 0; i < guessedWord.Length; i++)
+        {
+            guessedWord[i] = '_';
+        }
+        int attemptsLeft = 6;
+
+        Console.WriteLine("Вітаємо! Спробуйте вгадати слово!");
+        Console.WriteLine("Кількість літер у слові: " + secretWord.Length);
+        Console.WriteLine("Кількість можливих невірних спроб: " + attemptsLeft);
+
+    }
+}
